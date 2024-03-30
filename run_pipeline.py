d1a6b98175bd97fe73b98f58b5f18c7ddc3f9eea
adityajha0102 <adityajha0102@gmail.com>
2024-03-30 17:53:02 +0530

    Changed the dataset

--- run_pipeline.py
+++ run_pipeline.py
@@ -1,5 +1,5 @@
 from pipelines.training_pipeline import train_pipeline
 
-if __name__=="__main__":
+if __name__ == "__main__":
     # Run the pipeline
-    train_pipeline(data_path=r"D:\Projects\ShopScore\data\merged_data\preprocessed_data.csv")
+    train_pipeline(data_path=r"D:\Projects\ShopScore\data\merged_data\merged_data.csv")
