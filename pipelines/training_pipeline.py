f1bd6db6a3c9bd391e962a0a4472e851de8f8be1
adityajha0102 <adityajha0102@gmail.com>
2024-03-30 17:59:53 +0530

    Added work flow for all the steps

--- pipelines/training_pipeline.py
+++ pipelines/training_pipeline.py
@@ -8,6 +8,6 @@ from steps.evaluation import evaluate_model
 @pipeline(enable_cache=True)
 def train_pipeline(data_path: str):
     df = ingest_df(data_path)
-    clean_df(df)
-    train_model(df)
-    evaluate_model(df)
+    X_train, X_test, y_train, y_test = clean_df(df)
+    model = train_model(X_train, y_train)
+    fscore = evaluate_model(model, X_train, X_test, y_train, y_test)
